5b0df0427db412715661ab30a774de22718fc972
ZelalDev <152691825+ZelalDev@users.noreply.github.com>
2026-08-10 01:47:14 +0300

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> a;
+        for(int i=0;i<nums.size();i++){
+            int y=target-nums[i];
+            if(a.contains(y))
+            return {a[y],i};
+            else a[nums[i]]=i;
+        }
+        return {};
+    }
+};
